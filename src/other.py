c9ec30bdf826c53a8322efa4a90b839f66d3659e
z5258638 <z5258638@cse.unsw.edu.au>
2021-03-02 16:13:54 +1100

    added clear function to other.py

--- src/other.py
+++ src/other.py
@@ -1,5 +1,17 @@
+'''
+This file contains other functions relating to the implementation of 
+the backend project. Currently contains the clear function
+'''
+
+from data import data
+
 def clear_v1():
-    pass
+    '''
+    Clears all data from the dataframe
+    '''
+    data['users'].clear()
+    data['channels'].clear()
+    
 
 def search_v1(auth_user_id, query_str):
     return {
