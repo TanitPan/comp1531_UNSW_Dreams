1a1180f4a1c1ab87d720a609e4c0a3559e9fd1a8
Uditha <z5254719@unsw.edu.au>
2021-03-04 21:55:41 +1100

    Added tests determining the types, empty input parameters and unauthorised users

--- tests/channels_list_v1_test.py
+++ tests/channels_list_v1_test.py
@@ -19,23 +19,24 @@ def test_channels_listall_element_type():
     clear_v1()
     authorised_token = auth_register_v1('comp1531@student.com', 'student1531', 
                        'john', 'smith')
-    dict_channel = channels_listall_v1(authorised_token)
+    dict_channel = channels_list_v1(authorised_token)
     for element in dict_channel['channels']:
         assert(isinstance(element, dict) == True) 
         
 # Tests the function when there are no input parameters and raises an InputError
-def channels_list_empty_parameters():
+# need to clarify if this is possible
+"""def channels_list_empty_parameters():
     with pytest.raises(InputError):
-        assert channels_list_v1()
+        assert channels_list_v1()"""
 
 # Tests that the channels created by an unauthorised user is not listed as 
 def test_channels_listall_multiplechannel_length():
     clear_v1()
     authorised_token = auth_register_v1('user1@yahoo.com', 'password1234', 
                        'anne', 'smith')
-    non-authorised_token = authorised_token + 1
+    non_authorised_token = authorised_token['auth_user_id'] + 1
     channel_id1 = channels_create_v1(authorised_token, 'Channel0', True)
     channel_id2 = channels_create_v1(non_authorised_token, 'Channel1', True)
-    assert (len(channels_list_v1(authorised_token)) = 1
+    assert (len(channels_list_v1(authorised_token)) == 1)
 
 
