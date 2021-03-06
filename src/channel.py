e28fc4ffeaa5115ef3ef9033d71f6a98be62330f
Tanit Pan-anuruk <t.pan-anuruk@student.unsw.edu.au>
2021-03-06 19:12:08 +1100

    implement channel_join function and preliminary test case

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,6 @@
+from src.error import InputError, AccessError
+from data import data
+
 def channel_invite_v1(auth_user_id, channel_id, u_id):
     return {
     }
@@ -40,6 +43,55 @@ def channel_leave_v1(auth_user_id, channel_id):
     }
 
 def channel_join_v1(auth_user_id, channel_id):
+    """
+    This function adds a auth_user_id to a
+    channel_id provided.
+    """
+    # Flags to check for invalid input or invalid access
+    valid_auth = False
+    valid_channel = False
+    new_user_info = {}
+
+    # Loop to check if auth_user_id is valid and store user info
+    for user in data["users"]:
+        if user["auth_user_id"] == auth_user_id:
+            valid_auth = True
+            global_permission = user["permission_id"]
+            new_user_info["auth_user_id"] = user["auth_user_id"]
+            new_user_info["name_first"] = user["name_first"]
+            new_user_info["name_last"] = user["name_last"]
+            break
+
+    # Loop to check if channel id is valid and store channel privacy
+    for channel in data["channels"]:
+        if channel["channel_id"] == channel_id:
+            valid_channel = True
+            channel_privacy = channel["is_public"]
+
+            # Loop to check if member is already in channel 
+            for member in channel["all_members"]:
+                if member["auth_user_id"] == auth_user_id:
+                    raise AccessError("Repetitive join! You are already a member of channel !")
+            break
+
+
+    # Raise exception when detect invalid auth_user_id
+    if not valid_auth:
+        raise AccessError("authorised user ID is invalid")
+
+    # Raise excpetion when detect invalid channel
+    if not valid_channel:
+        raise InputError("invalid channel ID")
+
+    # if global_permission != 1 and not channel_privacy:
+    # Check if the auth_user_id has permission to join channel
+    if global_permission == 1 or channel_privacy:
+        for channel in data["channels"]:
+            if channel["channel_id"] == channel_id:
+                channel["all_members"].append(new_user_info)
+    else:
+        raise AccessError("channel ID refer to a private channel")
+
     return {
     }
 
---
+++ tests/channel_join_test.py
@@ -0,0 +1,99 @@
+"""
+This file contains test functions for channel_join function
+in channel.py.
+"""
+import pytest
+from data import data
+from src.other import clear_v1
+from src.error import InputError, AccessError
+from src.channel import channel_join_v1, channel_details_v1
+from src.auth import auth_register_v1
+from src.channels import channels_create_v1
+
+def test_channel_join():
+    """
+    This function checks if the user is successfully
+    added to the channel and store in the data structure.
+    """
+
+    # Clear the data structure
+    clear_v1()
+
+    # Call other functions to create the data and store in data structure
+    auth_id1 = auth_register_v1("johnsmith@gmail.com", "123456", "john", "smith")
+    auth_id2 = auth_register_v1("harrypotter@gmail.com", "555555", "harry", "potter")
+
+    channel_id1 = channels_create_v1(auth_id1["auth_user_id"], "Chill Soc", True)
+    print(channel_id1)
+
+    channel_join_v1(auth_id2["auth_user_id"], channel_id1["channel_id"])
+
+    # Black box testing version in waiting
+    # assert channel_details_v1(auth_id2["auth_user_id"], channel_id1["channel_id"]) == {
+    #     'name': 'Chill Soc',
+    #     'owner_members': [
+    #         { 
+    #             'auth_user_id' : 0, 
+    #             'name_first' : 'john',
+    #             'name_last' : 'smith', 
+    #         }
+    #     ],
+    #     'all_members': [
+    #         { 
+    #             'auth_user_id' : 0, 
+    #             'name_first' : 'john',
+    #             'name_last' : 'smith', 
+    #         },
+    #         {
+    #             'auth_user_id': 1,
+    #             'name_first': 'harry',
+    #             'name_last': 'potter',
+    #         }
+    #     ],
+    # }
+
+
+    assert data["channels"] == [
+    {      
+
+		'channel_id' :  1, 
+
+		'name' :  'Chill Soc', 
+
+		'owner_members' : [
+            { 
+
+                'auth_user_id' : 1, 
+
+                'name_first' : 'john', 
+
+                'name_last' : 'smith', 
+
+            }
+
+        ], 
+
+        'all_members' : [
+            { 
+
+                'auth_user_id' : 1, 
+
+                'name_first' : 'john', 
+
+                'name_last' : 'smith', 
+
+            },
+            { 
+
+                'auth_user_id' : 2, 
+
+                'name_first' : 'harry', 
+
+                'name_last' : 'potter', 
+
+            },
+        ], 
+
+        'is_public' :  True, # Assumed to default to public 
+    }
+    ]
