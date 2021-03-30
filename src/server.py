db9c46e35e8a58ffedc27b7ded06f175e70a19be
z5258638 <z5258638@cse.unsw.edu.au>
2021-03-30 12:37:12 +1100

    created auth_register_v2 server route in server.py

--- src/server.py
+++ src/server.py
@@ -5,6 +5,8 @@ from flask_cors import CORS
 from src.error import InputError
 from src import config
 
+from src.auth import auth_register_v2
+
 def defaultHandler(err):
     response = err.get_response()
     print('response', err, err.get_response())
@@ -31,6 +33,19 @@ def echo():
     return dumps({
         'data': data
     })
+
+@APP.route("/auth/register_v2", methods=['POST'])
+def auth_register_server():
+    payload = request.json()
+    
+    email = payload['email']
+    password = payload['password']
+    name_first = payload['name_first']
+    name_last = payload['name_last']
+
+    return dumps(
+        auth_register_v2(email, password, name_first, name_last)
+    )
 
 if __name__ == "__main__":
     APP.run(port=config.port) # Do not edit this port
