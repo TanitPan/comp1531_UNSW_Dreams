31256a1258ab00b6a5b6fbbc541313abaa90138a
Uditha <z5254719@unsw.edu.au>
2021-04-01 23:40:53 +1100

    Fixed version to fit the updated specs, including adding messages and tokens

--- src/channels.py
+++ src/channels.py
@@ -1,10 +1,12 @@
+''' This file implements channels_list, channels_listall and channels_create '''
+
 from data import data
 from src.error import InputError, AccessError
-from src.auth import auth_register_v1
-from src.helper import check_valid_user
+from src.auth import auth_register_v2
+from src.helper import valid_token
 
 def channels_list_v1(auth_user_id):
-    """
+    '''
     Taking in the auth_user_id, this function returns all channels the 
     authorised user has access to 
     Arguments:
@@ -17,7 +19,7 @@ def channels_list_v1(auth_user_id):
     Return Value:
         Returns a list consisting of dictionaries, with the information about a 
         channel ('channel_id' and 'name')
-    """ 
+    ''' 
     # Check the user_id to ensure that they were included as a channel member 
     check_valid_user(auth_user_id)
    
@@ -40,7 +42,7 @@ def channels_list_v1(auth_user_id):
     }
        
 def channels_listall_v1(auth_user_id):
-    """ 
+    ''' 
     This function returns the total number of channels that have been created, 
     regardless of whether the user has been added to the group or not. 
 
@@ -55,7 +57,7 @@ def channels_listall_v1(auth_user_id):
     Return Value:
         Returns a list consisting of dictionaries, with the information about a 
         channel (likely to be 'channel_id' and 'name'
-    """
+    '''
 
     # An AccessError is raised when the auth_user_id passed is not authorised
     check_valid_user(auth_user_id)
@@ -75,13 +77,13 @@ def channels_listall_v1(auth_user_id):
         'channels': channels_list
     }
 
-def channels_create_v1(auth_user_id, name, is_public):
-    """ 
+def channels_create_v2(token, name, is_public):
+    ''' 
     This function creates a new channel for an authorised user, which is either 
     public or private (if the authorised user is not a global Dream user). 
 
     Arguments:
-        auth_user_id (int)  - input token that ensures a user that is given 
+        token (string)  - input token that ensures a user that is given 
                               access has been validated. 
         name (string)       - name of the channel. 
         is_public (boolean) - checks whether a channel is public (assumed to be
@@ -90,17 +92,17 @@ def channels_create_v1(auth_user_id, name, is_public):
     Exceptions:
         InputError  - Occurs when the name input is consists of more than 20 
                       characters as determined by the len function. 
-        AccessError - Occurs when the auth_user_id has not been authenicated
+        AccessError - Occurs when the token has not been authenicated
                       (appended to the data['users'] file)
         
     Return Value:
         Returns a dictionary consisting of the channel_id values provided the
-        auth_user_id is valid and the name is less than 20 characters in length. 
-    """
+        token is valid and the name is less than 20 characters in length. 
+    '''
 
-    # Calls the check_valid_user function to ensure the user_id is validated 
-    # (has been added to the list of users). An AccessError is else raised. 
-    check_valid_user(auth_user_id)
+    # Calls the valid_token function to ensure the token is validated, returning
+    # the user_id if it has been. An AccessError is else raised. 
+    auth_user_id = valid_token(token)
 
     # Raises InputError if the length of the name is greater than 20 characters
     if len(name) > 20:
@@ -115,6 +117,15 @@ def channels_create_v1(auth_user_id, name, is_public):
         if channel['channel_id'] > maximum_id:
             maximum_id = channel['channel_id']       
     new_id = maximum_id + 1
+    
+    # Iteratively add the messages id, ensuring it is one greater than the last
+    # added messages _id
+    current_messages_id = 0
+    for channel in data['channels']:
+        for messages in channel['messages']:
+            if messages['message_id'] > current_messages_id:
+                current_messages_id = messages['message_id']  
+    new_messages_id = current_messages_id + 1
 
     # Only the channel creator has been added as user. 
     owner_list = [{'auth_user_id': auth_user_id}]
@@ -122,11 +133,20 @@ def channels_create_v1(auth_user_id, name, is_public):
 
     # Added new channel data to the dataframe.
     new_channels = {
-			    'channel_id': new_id, 
-			    'name': name, 
-			    'owner_members': owner_list, 
-			    'all_members': all_member_list, 
-			    'is_public': is_public,
+        'channel_id': new_id, 
+        'name': name, 
+        'owner_members': owner_list, 
+        'all_members': all_member_list, 
+        'is_public': is_public,
+        'dm_id': 0, #assume default
+        'messages': [
+            {
+                'message_id' : new_messages_id, #Assuming this is greater than 0
+                'message' : "",
+                'timestamp' : "", 
+                'auth_user_id' : auth_user_id, 
+            }
+        ]
     }
 
     data['channels'].append(new_channels)
