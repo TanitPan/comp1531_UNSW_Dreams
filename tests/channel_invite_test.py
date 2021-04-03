4621af9914becbeb711aec2ee7c801f21260daf8
Tanit Pan-anuruk <t.pan-anuruk@student.unsw.edu.au>
2021-04-03 19:52:06 +1100

    fix pylint issue

--- tests/channel_invite_test.py
+++ tests/channel_invite_test.py
@@ -76,10 +76,9 @@ def test_channel_invite_except_user():
     clear_v1()
     # Call other functions to create the data and store in data structure
     auth_dict1 = auth_register_v2("johnsmith@gmail.com", "123456", "john", "smith")
-    auth_dict2 = auth_register_v2("harrypotter@gmail.com", "555555", "harry", "potter")
+    auth_register_v2("harrypotter@gmail.com", "555555", "harry", "potter")
 
     auth_token1 = auth_dict1["token"]
-    auth_id2 = auth_dict2["auth_user_id"]
     invalid_u_id = 2222
 
     channel_id1 = channels_create_v2(auth_token1, "Chill Soc", True)
@@ -124,7 +123,6 @@ def test_channel_invite_except_invalid_auth():
     auth_dict2 = auth_register_v2("harrypotter@gmail.com", "555555", "harry", "potter")
 
     auth_token1 = auth_dict1["token"]
-    auth_token2 = auth_dict2["token"]
     auth_id2 = auth_dict2["auth_user_id"]
 
     # Create invalid token for the test
@@ -135,7 +133,7 @@ def test_channel_invite_except_invalid_auth():
 
     # Test invalid auth_user_id case
     with pytest.raises(AccessError):
-        channel_invite_v2(invalid_token, channel_id1["channel_id"], auth_id2 )
+        channel_invite_v2(invalid_token, channel_id1["channel_id"], auth_id2)
 
 
 def test_channel_invite_except_repetitive():
@@ -147,10 +145,9 @@ def test_channel_invite_except_repetitive():
     clear_v1()
     # Call other functions to create the data and store in data structure
     auth_dict1 = auth_register_v2("johnsmith@gmail.com", "123456", "john", "smith")
-    auth_dict2 = auth_register_v2("harrypotter@gmail.com", "555555", "harry", "potter")
+    auth_register_v2("harrypotter@gmail.com", "555555", "harry", "potter")
 
     auth_token1 = auth_dict1["token"]
-    auth_token2 = auth_dict2["token"]
     auth_id1 = auth_dict1["auth_user_id"]
 
     channel_id1 = channels_create_v2(auth_token1, "Chill Soc", True)
