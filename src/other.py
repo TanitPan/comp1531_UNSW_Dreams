580760120f411429bb035f13c3e6902cc3c2fb1e
Uditha <z5254719@unsw.edu.au>
2021-04-05 01:27:23 +1000

    Fixed mistakes in code

--- src/other.py
+++ src/other.py
@@ -38,23 +38,22 @@ def clear_v1():
     
 
 def search_v2(token, query_str):
-    auth_user_id = valid_token(token)
+    auth_user_id = helper.valid_token(token)
     if len(query_str) > 1000:
         raise InputError("Query string is too long")
         
     # Created empty list
-	messages_list = []
-	for channel in data["channels"]:
-	    for member in channel['all_members']:
-            if auth_user_id == member['auth_user_id'] && channel_id == -1: 
-		        for message in channel["messages"]:
-			        if query_string in  message["message"]:
-				        mess_dict = {
-				            "message_id": message["message_id"] ,
-					        "u_id": message["auth_user_id"] , 
-					        "message": message["message"], 
-					        "time_created": message["timestamp"]
-				        }
-				        messages_list.append(mess_dict)
-	
-	return messages_list
+    messages_list = []
+    for channel in data["channels"]:
+        for member in channel['all_members']:
+            if auth_user_id == member['auth_user_id'] and channel["dm_id"] == -1: 
+                for message in channel["messages"]:
+                    if query_str in  message["message"]:
+                        mess_dict = {
+                            "message_id": message["message_id"] ,
+                            "u_id": message["auth_user_id"] , 
+                            "message": message["message"], 
+                            "time_created": message["timestamp"],
+                        }
+                        messages_list.append(mess_dict)
+    return messages_list
