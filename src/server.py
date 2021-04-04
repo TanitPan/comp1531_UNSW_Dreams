ec4b224b38497afaa0c33dca8168c361d809c5e7
Uditha <z5254719@unsw.edu.au>
2021-04-04 21:11:35 +1000

    New routes have been added

--- src/server.py
+++ src/server.py
@@ -10,7 +10,7 @@ from src.admin import admin_user_remove_v1, admin_userpermission_change_v1
 from src.channels import channels_create_v2, channels_list_v2, channels_listall_v2
 from src.user import user_profile_v2, user_profile_setname_v2, user_profile_setemail_v2, user_profile_sethandle_v1
 from src.other import users_all_v1, clear_v1
-from src.channel import channel_invite_v2, channel_addowner_v1, channel_removeowner_v1
+from src.channel import channel_invite_v2, channel_addowner_v1, channel_removeowner_v1, channel_leave_v1
 
 def defaultHandler(err):
     response = err.get_response()
@@ -202,7 +202,14 @@ def channel_removeowner_server():
         channel_removeowner_v1(token, channel_id, u_id)
     )
 
-
+@APP.route("/channel/leave/v1", methods = ['POST'])
+def channel_leave_server():
+    payload = request.get_json()
+    token = payload["token"]
+    channel_id = payload["channel_id"]
+    return dumps(
+        channel_leave_v1(token, channel_id)
+    )
 
 
 
