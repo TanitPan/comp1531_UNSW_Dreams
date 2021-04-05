fab2ff6031bbc7b2375b41609ba856ec0afa12b5
Muhammad Khalik Humar <z5190328@ad.unsw.edu.au.com>
2021-04-06 00:53:42 +1000

    Added more tests

--- tests/message_test.py
+++ tests/message_test.py
@@ -7,7 +7,7 @@ from src.error import *
 from src.other import clear_v1
 
 def test_message_send():
-    clear_v1
+    clear_v1()
 
     u_1 = auth_register_v2('johnsmith@gmail.com', 'pass123456', 'John', 'Smith')
     channel_1 = channels_create_v2(u_1['token'], 'Channel1', True)
@@ -20,8 +20,9 @@ def test_message_send():
     assert message_1['messages'][0]['message'] == 'Hello'
     assert message_1['end'] == -1
 
+
 def test_message_send_multiple():
-    clear_v1
+    clear_v1()
 
     u_2 = auth_register_v2('janesmith@gmail.com', 'pass123456', 'Jane', 'Smith')
     channel_2 = channels_create_v2(u_2['token'], 'Channel2', True)
@@ -47,7 +48,7 @@ def test_message_send_multiple():
 
 
 def test_message_send_mult_users():
-    clear_v1
+    clear_v1()
 
     u_3 = auth_register_v2('banesmith@gmail.com', 'pass123456', 'Bane', 'Smith')
     u_4 = auth_register_v2('manesmith@gmail.com', 'pass78910', 'Mane', 'Smith')
@@ -79,50 +80,72 @@ def test_message_send_mult_users():
 
     assert message_3['end'] == -1
 
-def test_message_delete():
-    clear_v1
+def test_message_send_unauthorized():
+    clear_v1()
 
-    u_5 = auth_register_v2('batman@gmail.com', 'alfred123', 'Bruce', 'Wayne')
+    u_5 = auth_register_v2('greensquare@gmail.com', 'gs12345@', 'Green', 'Square')
+    u_6 = auth_register_v2('mascot@gmail.com', 'mascot12345@', 'Mascot', 'Mascot')
     channel_4 = channels_create_v2(u_5['token'], 'Channel4', True)
-    message_id_1 = message_send_v2(u_5['token'], channel_4['channel_id'], "Hello")
-    message_id_2 = message_send_v2(u_5['token'], channel_4['channel_id'], "World")
-    message_id_3 = message_send_v2(u_5['token'], channel_4['channel_id'], "COMP1531")
 
-    message_remove_v1(u_5['token'],message_id_2['message_id'])
+    with pytest.raises(AccessError):
+        message_send_v2(u_6['token'], channel_4['channel_id'], "Hello")
 
-    message_4 = channel_messages_v2(u_5['token'], channel_4['channel_id'], 0)
+def test_message_send_long_1000():
+    clear_v1()
+
+    u_7 = auth_register_v2('kingsford@gmail.com', 'kf12345@', 'Kings', 'Ford')
+    channel_5= channels_create_v2(u_7['token'], 'Channel5', True)
+
+    msg = "A" * 1001
+
+    with pytest.raises(InputError):
+        message_send_v2(u_7['token'], channel_5['channel_id'], msg)
+
+def test_message_delete():
+    clear_v1()
+
+    u_8 = auth_register_v2('batman@gmail.com', 'alfred123', 'Bruce', 'Wayne')
+    channel_6 = channels_create_v2(u_8['token'], 'Channel6', True)
+    message_id_1 = message_send_v2(u_8['token'], channel_6['channel_id'], "Hello")
+    message_id_2 = message_send_v2(u_8['token'], channel_6['channel_id'], "World")
+    message_id_3 = message_send_v2(u_8['token'], channel_6['channel_id'], "COMP1531")
+
+    message_remove_v1(u_8['token'],message_id_2['message_id'])
+
+    message_5 = channel_messages_v2(u_8['token'], channel_6['channel_id'], 0)
     
-    assert message_4['messages'][0]['message_id'] == message_id_3['message_id']
-    assert message_4['messages'][1]['message_id'] == message_id_1['message_id']
-
-    assert message_4['messages'][0]['auth_user_id'] == u_5['auth_user_id']
-    assert message_4['messages'][1]['auth_user_id'] == u_5['auth_user_id']
-
-    assert message_4['messages'][0]['message'] == 'COMP1531'
-    assert message_4['messages'][1]['message'] == 'Hello'
-
-    assert message_4['end'] == -1
-
-def test_message_edit():
-    clear_v1
-
-    u_6 = auth_register_v2('alfred@gmail.com', 'batman123', 'alfred', 'alfred')
-    channel_5 = channels_create_v2(u_6['token'], 'Channel4', True)
-    message_id_1 = message_send_v2(u_6['token'], channel_5['channel_id'], "Hello")
-    message_id_2 = message_send_v2(u_6['token'], channel_5['channel_id'], "World")
-    
-    message_edit_v1(u_6['token'],message_id_2['message_id'], "COMP1531")
-
-    message_5 = channel_messages_v2(u_6['token'], channel_5['channel_id'], 0)
-    
-    assert message_5['messages'][0]['message_id'] == message_id_2['message_id']
+    assert message_5['messages'][0]['message_id'] == message_id_3['message_id']
     assert message_5['messages'][1]['message_id'] == message_id_1['message_id']
 
-    assert message_5['messages'][0]['auth_user_id'] == u_6['auth_user_id']
-    assert message_5['messages'][1]['auth_user_id'] == u_6['auth_user_id']
+    assert message_5['messages'][0]['auth_user_id'] == u_8['auth_user_id']
+    assert message_5['messages'][1]['auth_user_id'] == u_8['auth_user_id']
 
     assert message_5['messages'][0]['message'] == 'COMP1531'
     assert message_5['messages'][1]['message'] == 'Hello'
 
     assert message_5['end'] == -1
+
+def test_message_edit():
+    clear_v1()
+
+    u_9 = auth_register_v2('alfred@gmail.com', 'batman123', 'alfred', 'alfred')
+    channel_7 = channels_create_v2(u_9['token'], 'Channel7', True)
+    message_id_1 = message_send_v2(u_9['token'], channel_7['channel_id'], "Hello")
+    message_id_2 = message_send_v2(u_9['token'], channel_7['channel_id'], "World")
+    
+    message_edit_v1(u_9['token'],message_id_2['message_id'], "COMP1531")
+
+    message_6 = channel_messages_v2(u_9['token'], channel_7['channel_id'], 0)
+    
+    assert message_6['messages'][0]['message_id'] == message_id_2['message_id']
+    assert message_6['messages'][1]['message_id'] == message_id_1['message_id']
+
+    assert message_6['messages'][0]['auth_user_id'] == u_9['auth_user_id']
+    assert message_6['messages'][1]['auth_user_id'] == u_9['auth_user_id']
+
+    assert message_6['messages'][0]['message'] == 'COMP1531'
+    assert message_6['messages'][1]['message'] == 'Hello'
+
+    assert message_6['end'] == -1
+
 
