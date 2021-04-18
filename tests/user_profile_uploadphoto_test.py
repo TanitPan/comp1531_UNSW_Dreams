48ed20846b8ad174286ed65db8e1f95a786a2205
z5258638 <z5258638@cse.unsw.edu.au>
2021-04-18 16:27:26 +1000

    added test cases for uploadphoto

---
+++ tests/user_profile_uploadphoto_test.py
@@ -0,0 +1,47 @@
+"""
+This file contains the tests for the implementation of user_profile_uploadphoto
+"""
+
+import pytest
+from src.error import InputError
+from src.auth import auth_register_v2
+from src.user import user_profile_v2, user_profile_uploadphoto_v1
+from src.other import clear_v1
+from src import config
+
+@pytest.fixture
+def register_user():
+    clear_v1()
+    user = auth_register_v2("johnsmith@gmail.com", "123456", "john", "smith")
+    token = user['token']
+    id = user['auth_user_id']
+    return token, id
+
+def test_valid_input(register_user):
+    token, id = register_user
+    url_path = config.url
+    cute_cat_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Khaomanee_cat.jpg/1200px-Khaomanee_cat.jpg"
+    res = user_profile_uploadphoto_v1(token, url_path, cute_cat_url, 50, 50, 1000, 1000)
+    assert res == {}
+    profile = user_profile_v2(token, id)
+    assert profile['user']['profile_img_url'] != config.url + 'default.jpg'
+
+def test_invalid_inputs(register_user):
+    token, id = register_user
+    url_path = config.url
+    fake_url = "not_a_url"
+    # test invalid url
+    with pytest.raises(InputError):
+        user_profile_uploadphoto_v1(token, url_path, fake_url, 50, 50, 1000, 1000)
+    # test invalid cropping dimensions
+    scottish_fold_kitty = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Scottish_fold_cat.jpg/220px-Scottish_fold_cat.jpg"
+    with pytest.raises(InputError):
+        user_profile_uploadphoto_v1(token, url_path, scottish_fold_kitty, 50, 50, 5000, 5000)
+    # test inversed cropping dimensions
+    with pytest.raises(InputError):
+        user_profile_uploadphoto_v1(token, url_path, scottish_fold_kitty, 5000, 5000, 50, 50)
+    # test non-jpg file
+    png_file_cat = "https://icatcare.org/app/uploads/2018/09/Scottish-fold-2.png"
+    # test invalid url
+    with pytest.raises(InputError):
+        user_profile_uploadphoto_v1(token, url_path, png_file_cat, 50, 50, 800, 800)
