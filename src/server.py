9396fc0239a3591b423d3e051840c3c41e09cdd5
Muhammad Khalik Humar <z5190328@ad.unsw.edu.au.com>
2021-04-20 06:06:28 +1000

    Update from master

--- src/server.py
+++ src/server.py
@@ -12,18 +12,10 @@ from src.user import (user_profile_v2, user_profile_setname_v2,
 user_profile_setemail_v2, user_profile_sethandle_v1, user_stats_v1, 
 user_profile_uploadphoto_v1)
 from src.other import users_all_v1, users_stats_v1, clear_v1, search_v2
-<<<<<<< HEAD
 from src.channel import channel_invite_v2, channel_addowner_v1, channel_removeowner_v1, channel_leave_v1, channel_join_v2, channel_details_v2, channel_messages_v2
-from src.dm import dm_create_v1, dm_list_v1
-from src.standup import standup_start_v1, standup_active_v1
-from src.message import message_send_v2, message_edit_v1, message_remove_v1, message_sendlater_v1, message_pin_v1, message_unpin_v1
-=======
-from src.channel import (channel_invite_v2, channel_addowner_v1, 
-channel_removeowner_v1, channel_leave_v1, channel_join_v2)
 from src.dm import dm_create_v1, dm_list_v1, dm_details_v1
 from src.standup import standup_start_v1, standup_active_v1, standup_send_v1
-
->>>>>>> master
+from src.message import message_send_v2, message_edit_v1, message_remove_v1, message_sendlater_v1, message_pin_v1, message_unpin_v1
 
 def defaultHandler(err):
     response = err.get_response()
